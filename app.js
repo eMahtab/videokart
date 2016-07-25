ddfa764a56e7217733d6fc53a3dd08ba7315c6f4
Mahtab Alam <mahtab@secondus.gamersdemokrasy.com>
2016-07-25 11:51:20 +0530

    Added request signing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express=require('express');
+const aws = require('aws-sdk');
 var app=express();
 
 var routes=require('./routes/route.js');
@@ -7,10 +8,38 @@ app.set('view engine','ejs');
 
 app.use(express.static(__dirname + '/public'));
 
+const S3_BUCKET = process.env.S3_BUCKET;
 
 app.get('/',routes.home);
 app.get('/gallery',routes.gallery);
 app.get('/upload',routes.upload);
+
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
 
 
 
